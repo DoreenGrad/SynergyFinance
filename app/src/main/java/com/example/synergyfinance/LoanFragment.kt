9185a0adc089e3bf4f118db68448e039da956f77
Lemmenasie <johannrossj5@gmail.com>
2021-07-30 16:10:38 +0200

    Synergy Finance Synergy Code
    Commit 06
    Lemmenasie
    30 July 2021

--- app/src/main/java/com/example/synergyfinance/LoanFragment.kt
+++ app/src/main/java/com/example/synergyfinance/LoanFragment.kt
@@ -4,10 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
 
 class LoanFragment : Fragment() {
+
+    private lateinit var seekBar: SeekBar
+    private lateinit var editText: EditText
+
+    /*private var listener: LoanFragmentListener? = null
+
+    interface LoanFragmentListener {
+        fun onInputSent(input: CharSequence)
+    }*/
 
     @Nullable
     override fun onCreateView(
@@ -15,7 +27,39 @@ class LoanFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val v = inflater.inflate(R.layout.fragment_loan, container, false)
 
-        return inflater.inflate(R.layout.fragment_loan, container, false)
+        //listener?.onInputSent("Loan Calculator")
+
+        seekBar = v.findViewById(R.id.seekBar)
+        editText = v.findViewById(R.id.editTextInterestRate)
+
+        seekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                editText.setText("$progress%")
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+        })
+
+        return v
     }
+
+    /*override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = if (context is LoanFragmentListener) {
+            context
+        } else {
+            throw RuntimeException(
+                context.toString()
+                        + " must implement LoanFragmentListener"
+            )
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }*/
 }
