cb56e01cb2ae5534d1e5237a61a4ab9ebca48c24
DoreenGrad <66521420+DoreenGrad@users.noreply.github.com>
2021-08-08 21:22:06 +0200

    Add files via upload

---
+++ main/java/com/example/studentsbudgetingapp/Activities/ForgotPasswordActivity.java
@@ -0,0 +1,17 @@
+package com.example.studentsbudgetingapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studentsbudgetingapp.R;
+
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
